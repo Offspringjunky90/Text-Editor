a17ab59645771f6b98b31abf7c888a30267f7247
Robert Augusta <RobAAugusta@gmail.com>
2022-02-05 17:43:11 -0500

    Push to Heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const objects = await store.getAll();
+  const contenttoWrite = objects.length > 0 ? { content: content, id: objects[0].id } : { content: content };
+  const request = await store.put(contenttoWrite);
+  const result = await request;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  if (result.length > 0) {
+      console.log("result.value", result);
+      console.log(result[0].content);
+      return result[0].content;
+  } else {
+      return null;
+  }
+};
 
 initdb();
